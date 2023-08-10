f1d04f5eeebc0cdd6367778f15e9a0c6a4c6abb3
Jarod Florence <jarod.a.florence@gmail.com>
2023-08-10 19:53:04 +0000

    implement rate limiting and added readme

--- README.md
+++ README.md
@@ -1,10 +1,79 @@
-# GitHub Codespaces ♥️ Flask
+# Grow Therapy Wikipedia API Wrapper
 
-Welcome to your shiny new Codespace running Flask! We've got everything fired up and running for you to explore Flask.
+This Flask-based web application provides a wrapper around the Wikipedia Pageviews API to retrieve article views data for specified durations.
 
-You've got a blank canvas to work on from a git perspective as well. There's a single initial commit with the what you're seeing right now - where you go from here is up to you!
+## Requirements:
+- Flask
+- requests
 
-Everything you do here is contained within this one codespace. There is no repository on GitHub yet. If and when you’re ready you can click "Publish Branch" and we’ll create your repository and push up your project. If you were just exploring then and have no further need for this code then you can simply delete your codespace and it's gone forever.
+## Installation & Running:
+
+1. Clone the repository.
+2. Install the required libraries:
+    ```bash
+    pip install Flask requests
+    ```
+
+3. Run the Flask application:
+    ```bash
+    python app.py
+    ```
+
+## API Endpoints:
+
+### 1. Top Articles
+Fetches the top articles for a given date and specified duration (`week` or `month`).
+
+**Endpoint:** `/top-articles/<int:year>/<int:month>/<int:day>`
+
+**Parameters:**
+- `year`, `month`, `day`: The starting date from which the articles will be fetched.
+- `duration`: Either `week` or `month` to specify the range.
+
+**Usage:**
+```
+/top-articles/2015/10/10?duration=week
+```
+
+### 2. Article Views
+Retrieves the view count for a specific article over a given duration (`week` or `month`).
+
+**Endpoint:** `/article-views`
+
+**Parameters:**
+- `title`: The title of the article.
+- `duration`: Either `week` or `month` to specify the range.
+
+**Usage:**
+```
+/article-views?title=Napoleon&duration=week
+```
+
+### 3. Max Views Day
+Finds the day within a given duration (`week` or `month`) when a specific article got the most page views.
+
+**Endpoint:** `/max-views-day`
+
+**Parameters:**
+- `title`: The title of the article.
+- `duration`: Either `week` or `month` to specify the range.
+
+**Usage:**
+```
+/max-views-day?title=Napoleon&duration=week
+```
+
+## Notes:
+- The application fetches data from the Wikipedia Pageviews API. There might be rate limits applied, so if you're fetching data for a longer duration, it might take some time.
+- Ensure you're querying dates for which Wikipedia has relevant data.
+
+## Future Improvements:
+- Implement caching mechanisms to store data and avoid repeated requests to the Wikipedia API.
+- Enhance error handling to address possible data-fetching issues.
+- Extend the API to cater for more functionalities and possibly other Wikimedia data.
+
+## Feedback:
+For any feedback or issues, please raise a ticket or send an email. Contributions are also welcomed through pull requests.
 
 To run this application:
 
--- app.py
+++ app.py
@@ -1,7 +1,141 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
+import requests, time
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+BASE_URL = "https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access"
+MAX_RETRIES = 3
+DELAY_BETWEEN_REQUESTS = 2  # in seconds
+
+def fetch_data(date):
+    url = f"{BASE_URL}/{date.strftime('%Y/%m/%d')}"
+    print(url)
+
+    retries = 0
+    while retries < MAX_RETRIES:
+        response = requests.get(url)
+        print(response)
+        print(retries)
+        
+        if response.status_code == 403:  # Forbidden, probably due to rate limiting
+            print("Rate limited. Waiting before retrying...")
+            time.sleep(DELAY_BETWEEN_REQUESTS)
+            retries += 1
+        elif response.status_code == 200:  # Success
+            try:
+                return response.json()
+            except requests.exceptions.JSONDecodeError:
+                print("JSON decode error occurred.")
+                return {}
+        else:
+            print(f"Unexpected status code: {response.status_code}. Breaking out.")
+            break
+
+    return {}
+
+@app.route('/top-articles/<int:year>/<int:month>/<int:day>')
+def top_articles(year, month, day):
+    # Parsing the provided date
+    try:
+        start_date = datetime(year, month, day).date()
+    except ValueError:
+        return jsonify(error="Invalid date provided."), 400
+
+    # Getting the duration and verifying it
+    duration = request.args.get('duration')
+    if duration not in ["week", "month"]:
+        return jsonify(error="Invalid duration. Please select either 'week' or 'month'."), 400
+
+    # Calculate the end date based on the provided start date and duration
+    if duration == "week":
+        end_date = start_date + timedelta(days=6)  # Including the start date
+    else:
+        end_date = start_date + timedelta(days=29)  # Including the start date
+
+    articles = {}
+    current_date = start_date
+    while current_date <= end_date:
+        data = fetch_data(current_date)
+        print(jsonify(data))
+        articles_data = data.get('items', [])
+        if articles_data:
+            for article in articles_data[0].get('articles', []):
+
+                title = article['article']
+                views = article['views']
+                articles[title] = articles.get(title, 0) + views
+            current_date += timedelta(days=1)
+
+    sorted_articles = sorted(articles.items(), key=lambda x: x[1], reverse=True)
+    return jsonify(sorted_articles)
+
+
+@app.route('/article-views')
+def article_views():
+    title = request.args.get('title')
+    if not title:
+        return jsonify(error="Article title is required."), 400
+
+    duration = request.args.get('duration')
+    if duration not in ["week", "month"]:
+        return jsonify(error="Invalid duration. Please select either 'week' or 'month'."), 400
+
+    # Calculate the start and end dates based on the duration
+    end_date = datetime.now().date()
+    if duration == "week":
+        start_date = end_date - timedelta(days=7)
+    else:
+        start_date = end_date - timedelta(days=30)
+
+    total_views = 0
+    current_date = start_date
+    while current_date <= end_date:
+        data = fetch_data(current_date)
+        for article in data.get('items', [{}])[0].get('articles', []):
+            if article['article'] == title:
+                total_views += article['views']
+                break
+        current_date += timedelta(days=1)
+
+    return jsonify({"title": title, "total_views": total_views})
+
+@app.route('/max-views-day')
+def max_views_day():
+    title = request.args.get('title')
+    if not title:
+        return jsonify(error="Article title is required."), 400
+
+    duration = request.args.get('duration')
+    if duration not in ["week", "month"]:
+        return jsonify(error="Invalid duration. Please select either 'week' or 'month'."), 400
+
+    # Calculate the start and end dates based on the duration
+    end_date = datetime.now().date()
+    if duration == "week":
+        start_date = end_date - timedelta(days=7)
+    else:
+        start_date = end_date - timedelta(days=30)
+
+    max_views = 0
+    max_views_date = None
+    current_date = start_date
+    while current_date <= end_date:
+        data = fetch_data(current_date)
+        for article in data.get('items', [{}])[0].get('articles', []):
+            if article['article'] == title:
+                views = article['views']
+                if views > max_views:
+                    max_views = views
+                    max_views_date = current_date
+                break
+        current_date += timedelta(days=1)
+
+    return jsonify({
+        "title": title,
+        "max_views": max_views,
+        "max_views_date": max_views_date.strftime('%Y-%m-%d') if max_views_date else None
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
